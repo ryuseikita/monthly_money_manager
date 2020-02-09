5589b1178e5dcfbb26b65baf6390f0f8881ffa3b
RysSuzuki <suzukiryusei@RYS-MacBook.local>
2020-02-09 23:49:33 +0900

    :sparkles: add：コメントの削除機能追加

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,6 @@
 class ReviewsController < ApplicationController
+  before_action :set_review,only:[:destroy,:edit,:update]
+
   def create
     @service = Service.find(params[:service_id])
     @review = @service.reviews.build(review_params)
@@ -12,9 +14,22 @@ class ReviewsController < ApplicationController
       end
     end
   end
+
+  def destroy
+    if current_user.id == @review.user_id
+      @review.comment = "コメントは削除されました。"
+      @review.save
+    end
+    redirect_to service_path(params[:service_id])
+  end
+
   private
 
   def review_params
-    params.require(:review).permit(:service_id, :user_id,:comment)
+    params.require(:review).permit(:service_id, :user_id, :comment)
+  end
+
+  def set_review
+    @review = Review.find(params[:id])
   end
 end
