0190fefea37ddea3b9546aba49a31c3ac399f200
RysSuzuki <suzukiryusei@RYS-MacBook.local>
2020-02-25 23:14:44 +0900

    :sparkles: add：delivery設定変更機能追加

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -1,2 +1,21 @@
 class DeliveriesController < ApplicationController
+  before_action :set_delivery,only:[:update]
+
+  def update
+    if @delivery.update(delivery_params)
+        redirect_to permanths_path, notice: "userを編集しました！" 
+    else
+        redirect_to edit_user_path(@delivery.user_id)
+    end
+  end
+
+  private
+
+  def delivery_params
+      params.require(:delivery).permit(:mail_flag, :slack_flag, :linebot_flag)
+  end
+
+  def set_delivery
+      @delivery = Delivery.find(current_user.id)
+  end
 end
