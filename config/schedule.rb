98df555ada2ee89b563d682d22b3da815de2ab5b
RysSuzuki <suzukiryusei@RYS-MacBook.local>
2020-03-04 22:40:03 +0900

    :arrows_clockwise: change: タスク実行時間の修正

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,6 +2,10 @@ set :output, 'log/crontab.log'
 ENV['RAILS_ENV'] ||= 'development'
 set :environment, ENV['RAILS_ENV']
 
-every 1.day, at: '0:00 am' do
+every 1.day, at: '1:00 am' do
    rake 'notification:permanth_money'
- end
+end
+
+every 1.day, at: '0:00 am' do
+  rake 'notification:cancellation_service'
+end
