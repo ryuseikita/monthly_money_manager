cb820cf0e7dc96364dbf70a6bb44f5a81090b5ae
RysSuzuki <suzukiryusei@RYS-MacBook.local>
2020-03-15 15:02:44 +0900

    :zzz: disable：エラー修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,9 +4,6 @@ CarrierWave.configure do |config|
   require 'carrierwave/storage/fog'
   # config.root = Rails.root.join('tmp')
   # config.cache_dir = "#{Rails.root}/tmp/uploads"
-  config.cache_storage = :fog
-  config.storage :fog
-  config.fog_provider = 'fog/aws'
   config.fog_credentials = {
       provider:              'AWS',
       aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
@@ -14,7 +11,8 @@ CarrierWave.configure do |config|
       region:                'ap-northeast-1',
       path_style:            true,
   }
-
+  config.cache_storage = :fog
+  config.storage :fog
   config.fog_public     = true
   config.fog_attributes = {'Cache-Control' => 'public, max-age=86400'}
 
