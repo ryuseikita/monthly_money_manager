cbbe9afad4829e86abf61f7a86b97edb31f742ac
RysSuzuki <suzukiryusei@RYS-MacBook.local>
2020-03-16 23:26:50 +0900

    :arrows_clockwise: change：ローカル環境でのファイルアップロードはpublicを利用

--- app/uploaders/icon_uploader.rb
+++ app/uploaders/icon_uploader.rb
@@ -1,6 +1,6 @@
 class IconUploader < CarrierWave::Uploader::Base
   if Rails.env.development?
-    storage :fog
+    storage :file
   elsif Rails.env.test?
     storage :file
   else
--- app/uploaders/service_icon_uploader.rb
+++ app/uploaders/service_icon_uploader.rb
@@ -1,6 +1,6 @@
 class ServiceIconUploader < CarrierWave::Uploader::Base
   if Rails.env.development?
-    storage :fog
+    storage :file
   elsif Rails.env.test?
     storage :file
   else
