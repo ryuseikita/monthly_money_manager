275ad510a2733a1668e3a6e5d372c388d1b3d76b
RysSuzuki <suzukiryusei@RYS-MacBook.local>
2020-03-22 23:48:09 +0900

    :sparkles: add： 初期テストデータ作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,10 +17,42 @@ User.create!(name: 'admin',
 Delivery.create!(user_id: 1
                 )
 User.create!(name: 'testuser',
-                email: 'ryslsuzuki@gmail.com',
+                email: 'testuser@example.com',
                 password: 'password',
                 password_confirmation: 'password',
              )
- Delivery.create!(user_id: 2
-                 )
- 
+Delivery.create!(user_id: 2
+                )
+
+20.times do |n|
+    name = Faker::Name.name
+    email = "testuser#{n+2}@example.com"
+    password = "password"
+    User.create!(name:  name,
+                email:  email,
+                password:               password,
+                password_confirmation:  password
+                )
+end
+
+100.times do |n|
+  number = "#{rand(3)+1}"
+  if number == "1"
+    tmp = "高い"
+  elsif number == "2"
+    tmp = "安い"
+  else
+    tmp = "いいね！"
+  end
+  comment = tmp
+  user_id = "#{rand(20)+3}"
+  service_id = "#{rand(23)+1}"
+  Review.create!(user_id:    user_id,
+                service_id:  service_id,
+                comment:     comment
+                )
+end
+
+
+
+
