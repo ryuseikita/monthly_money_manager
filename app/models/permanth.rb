19be689d73e42b65119bda6eede24f5c3d62e56e
RysSuzuki <suzukiryusei@RYS-MacBook.local>
2020-03-22 21:43:05 +0900

    :arrow_heading_up: update： 解約予定日に過去の日付が入力できないように修正

--- app/models/permanth.rb
+++ app/models/permanth.rb
@@ -2,6 +2,15 @@ class Permanth < ApplicationRecord
   belongs_to :user
   belongs_to :service
 
-  validates  :user_id, presence: true
-  validates  :service_id, presence: true
+  validates  :user_id,
+                presence: true
+  validates  :service_id,
+                presence: true
+  validate   :cancellation_vaild
+
+  def cancellation_vaild
+    if cancellation.present?
+      errors.add(:cancellation, 'は翌日以降日付を入力してください') if cancellation < Date.today
+    end
+  end
 end
