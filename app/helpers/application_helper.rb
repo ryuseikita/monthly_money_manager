acd74c8402de4faa0c0f4188cb44e6d5513502dd
RysSuzuki <suzukiryusei@RYS-MacBook.local>
2020-04-21 23:28:29 +0900

    :dizzy: clean：役割ごとにファイルを分割

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,4 @@
 module ApplicationHelper
-  def current_user
-    if (user_id = session[:user_id])
-      @current_user ||= User.find_by(id: user_id)
-    elsif (user_id = cookies.signed[:user_id])
-      user = User.find_by(id: user_id)
-      if user && user.authenticated?(cookies[:remember_token])
-        session[:user_id] = user.id
-        @current_user = user
-      end
-    end
-  end
-
 
   def logged_in?
     current_user.present?
@@ -32,17 +20,4 @@ module ApplicationHelper
     return sum
   end
 
-    # ユーザーのセッションを永続的にする
-  def remember(user)
-    user.remember
-    cookies.permanent.signed[:user_id] = user.id
-    cookies.permanent[:remember_token] = user.remember_token
-  end
-
-  def forget(user)
-    user.forget
-    cookies.delete(:user_id)
-    cookies.delete(:remember_token)
-  end
-  
 end
