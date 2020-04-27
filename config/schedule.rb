5a9184d45d415b722df554a0dd0fc3ed692296d0
RysSuzuki <suzukiryusei@RYS-MacBook.local>
2020-04-27 18:36:22 +0900

    :warning: fix：cronが実行できない不具合修正1

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,9 +1,11 @@
+ENV.each { |k, v| env(k, v) } 
+
 set :output, 'log/crontab.log'
-ENV['RAILS_ENV'] ||= 'development'
+ENV['RAILS_ENV'] ||= 'production'
 set :environment, ENV['RAILS_ENV']
 
 # crontab型の設定「分」「時」「日」「月」「曜日」
-every '0 12 27 * *' do
+every '0 8 25 * *' do
    rake 'notification:permanth_money'
 end
 
